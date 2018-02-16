bed5ae1316136c741f02fc50f9b16b33b6a5fb45
Ian Holden <ian.holden@mediamiser.com>
2018-02-16 12:24:13 -0500

    WIP Save
    - Added navigation menu, with highlighting selected option
    - Main template now display different body info, based upon navigation
    - Skeleton of Report service

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
+use App\Service\ReportService;
 use App\Service\ScheduledGameService;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class DefaultController
@@ -13,13 +14,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="default")
+     * Display default page
+     *
+     * @Route(
+     *     "/",
+     *     name="about"
+     * )
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render(
+            'base.html.twig',
+            array(
+                'title' => "Softball Scheduler",
+                'module' => "about",
+            )
+        );
+    }
+
+    /**
+     * Display full league schedule in calendar format
+     *
+     * @Route(
+     *     "/schedule",
+     *     name="calendar_schedule"
+     * )
      *
      * @param ScheduledGameService $scheduledGameService
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(ScheduledGameService $scheduledGameService)
+    public function scheduleAction(ScheduledGameService $scheduledGameService)
     {
         $scheduledGamesData = $scheduledGameService->dumpSchedule();
 
@@ -27,7 +54,34 @@ class DefaultController extends Controller
             'base.html.twig',
             array(
                 'title' => "Softball Scheduler",
+                'module' => "calendar_schedule",
                 'schedule' => $scheduledGamesData,
+            )
+        );
+    }
+
+    /**
+     * Display Home and Away report
+     *
+     * @Route(
+     *     "/report_home_n_away",
+     *     name="report_home_and_away"
+     * )
+     *
+     * @param ReportService $reportService
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function reportHomeAndAwayAction(ReportService $reportService)
+    {
+        $reportData = $reportService->HomeAndAway();
+
+        return $this->render(
+            'base.html.twig',
+            array(
+                'title' => "Softball Scheduler",
+                'module' => 'report_home_and_away',
+                'report' => $reportData,
             )
         );
     }
---
+++ src/Service/ReportService.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henry
+ * Date: 2018-02-16
+ * Time: 11:28 AM
+ */
+
+namespace App\Service;
+
+
+/**
+ * Class ReportService
+ * @package App\Service
+ */
+class ReportService
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function homeAndAway()
+    {
+        $reportData = array();
+
+        return $reportData;
+    }
+}
